fecae6246e2a84ca30bf52e35ef9fb28bf681960
Luc De Meyer <luc.demeyer@noviat.com>
2019-04-08 12:56:51 +0200

    init cycle - usability improvements

--- account_ebics/__manifest__.py
+++ account_ebics/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': 'EBICS banking protocol',
-    'version': '10.0.1.5.0',
+    'version': '10.0.1.6.0',
     'license': 'AGPL-3',
     'author': 'Noviat',
     'category': 'Accounting & Finance',
