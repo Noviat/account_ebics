078e3507509f4e0cc0d97e51244632747083c17e
Luc De Meyer <luc.demeyer@noviat.com>
2019-05-11 20:06:03 +0200

    [MIG] account_ebics_payment_order: Migration to 11.0

--- account_ebics_payment_order/__manifest__.py
+++ account_ebics_payment_order/__manifest__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
-# Copyright 2009-2017 Noviat.
+# Copyright 2009-2019 Noviat.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Upload Payment Order via EBICS',
-    'version': '10.0.1.0.0',
+    'version': '11.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Noviat',
     'category': 'Accounting & Finance',
