6dd7dd25bd85733036d49682d9b297636864c5c5
Luc De Meyer <luc.demeyer@noviat.com>
2019-05-11 20:06:03 +0200

    [MIG] account_ebics_payment_order: Migration to 11.0

--- account_ebics_payment_order/models/account_payment_order.py
+++ account_ebics_payment_order/models/account_payment_order.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2009-2017 Noviat.
+# Copyright 2009-2019 Noviat.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, models, _
