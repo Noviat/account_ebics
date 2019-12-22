ef14c963d9a5cc2aef69232775a05d7c055d140e
Luc De Meyer <luc.demeyer@noviat.com>
2019-12-22 10:54:00 +0100

    lpgl

--- account_ebics_oe/__manifest__.py
+++ account_ebics_oe/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2019 Noviat.
-# License LGPL-3 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3 or later (http://www.gnu.org/licenses/lpgl).
 
 {
     'name': 'account_ebics on Odoo Enterprise',
