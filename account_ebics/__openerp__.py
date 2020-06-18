09d9dbcf48e2213488ce29ce132d1583e361f783
Luc De Meyer <luc.demeyer@noviat.com>
2020-06-18 21:39:01 +0200

    increment version level

--- account_ebics/__openerp__.py
+++ account_ebics/__openerp__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright 2009-2018 Noviat.
+# Copyright 2009-2020 Noviat.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'EBICS banking protocol',
-    'version': '8.0.1.5.0',
+    'version': '8.0.1.5.1',
     'license': 'AGPL-3',
     'author': 'Noviat',
     'category': 'Accounting & Finance',
