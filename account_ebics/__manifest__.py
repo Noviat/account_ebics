c092e11b7667becb3019b2196fe89bb933058abb
Luc De Meyer <luc.demeyer@noviat.com>
2020-06-18 22:03:43 +0200

    increment version level

--- account_ebics/__manifest__.py
+++ account_ebics/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright 2009-2019 Noviat.
+# Copyright 2009-2020 Noviat.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'EBICS banking protocol',
-    'version': '11.0.1.7.0',
+    'version': '11.0.1.7.1',
     'license': 'AGPL-3',
     'author': 'Noviat',
     'category': 'Accounting & Finance',
