f18afcda3d1f3fd4336c778ab0f1aabf4071a6c5
Luc De Meyer <luc.demeyer@noviat.com>
2020-06-18 22:19:01 +0200

    ebics.file acl cru for group_account_invoice

--- account_ebics/__manifest__.py
+++ account_ebics/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'EBICS banking protocol',
-    'version': '13.0.1.0.0',
+    'version': '13.0.1.0.1',
     'license': 'LGPL-3',
     'author': 'Noviat',
     'category': 'Accounting & Finance',
