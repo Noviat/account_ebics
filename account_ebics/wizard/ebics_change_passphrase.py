2f8dac0a977b9d42add5e98cdb6cc1aa87cd6452
Luc De Meyer <luc.demeyer@noviat.com>
2020-06-24 23:39:33 +0200

    fix cfonb import

--- account_ebics/wizard/ebics_change_passphrase.py
+++ account_ebics/wizard/ebics_change_passphrase.py
@@ -1,9 +1,9 @@
-# Copyright 2009-2019 Noviat.
+# Copyright 2009-2020 Noviat.
 # License LGPL-3 or later (http://www.gnu.org/licenses/lpgl).
 
 import logging
 
-from odoo import api, fields, models, _
+from odoo import _, fields, models
 from odoo.exceptions import UserError
 
 _logger = logging.getLogger(__name__)
