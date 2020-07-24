5f159e33fb565cd16dce47ac607a575d11ce4360
Luc De Meyer <luc.demeyer@noviat.com>
2020-07-24 12:44:49 +0200

    [13.0]batch payment ebics.xfer wiz improvements

--- account_ebics_batch_payment/models/account_batch_payment.py
+++ account_ebics_batch_payment/models/account_batch_payment.py
@@ -1,7 +1,8 @@
 # Copyright 2009-2020 Noviat.
 # License LGPL-3 or later (http://www.gnu.org/licenses/lpgl).
 
-from odoo import _, api, models
+from odoo import _, models
+from odoo.exceptions import UserError
 
 
 class AccountBatchPayment(models.Model):
@@ -12,6 +13,17 @@ class AccountBatchPayment(models.Model):
         ctx = self.env.context.copy()
 
         origin = _("Batch Payment") + ": " + self.name
+        ebics_config = self.env['ebics.config'].search([
+            ('journal_ids', '=', self.journal_id.id),
+            ('state', '=', 'confirm'),
+        ])
+        if not ebics_config:
+            raise UserError(_(
+                "No active EBICS configuration available "
+                "for the selected bank."
+            ))
+        if len(ebics_config) == 1:
+            ctx["default_ebics_config_id"] = ebics_config.id
         ctx.update(
             {
                 "default_upload_data": self.export_file,
@@ -19,7 +31,9 @@ class AccountBatchPayment(models.Model):
                 "origin": origin,
             }
         )
+
         ebics_xfer = self.env["ebics.xfer"].with_context(ctx).create({})
+        ebics_xfer._onchange_ebics_config_id()
         ebics_xfer._onchange_upload_data()
         ebics_xfer._onchange_format_id()
         view = self.env.ref("account_ebics.ebics_xfer_view_form_upload")
