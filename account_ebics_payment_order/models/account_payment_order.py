236709eb16973028c4a32e5669e4b9f17099e594
Luc De Meyer <luc.demeyer@noviat.com>
2020-07-25 18:24:08 +0200

    [13.0]fix account_ebics_payment_order

--- account_ebics_payment_order/models/account_payment_order.py
+++ account_ebics_payment_order/models/account_payment_order.py
@@ -1,7 +1,7 @@
 # Copyright 2009-2020 Noviat.
 # License LGPL-3 or later (http://www.gnu.org/licenses/lpgl).
 
-from odoo import api, models, _
+from odoo import _, models
 from odoo.exceptions import UserError
 
 
@@ -26,12 +26,24 @@ class AccountPaymentOrder(models.Model):
                 "EBICS Processing > EBICS Upload menu"))
         else:
             origin = _("Payment Order") + ': ' + self.name
+            ebics_config = self.env['ebics.config'].search([
+                ('journal_ids', '=', self.journal_id.id),
+                ('state', '=', 'confirm'),
+            ])
+            if not ebics_config:
+                raise UserError(_(
+                    "No active EBICS configuration available "
+                    "for the selected bank."
+                ))
+            if len(ebics_config) == 1:
+                ctx["default_ebics_config_id"] = ebics_config.id
             ctx.update({
                 'default_upload_data': attach.datas,
-                'default_upload_fname': attach.datas_fname,
+                'default_upload_fname': attach.name,
                 'origin': origin,
             })
             ebics_xfer = self.env['ebics.xfer'].with_context(ctx).create({})
+            ebics_xfer._onchange_ebics_config_id()
             ebics_xfer._onchange_upload_data()
             ebics_xfer._onchange_format_id()
             view = self.env.ref('account_ebics.ebics_xfer_view_form_upload')
