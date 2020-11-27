305c7730f0f37ae5235d6272e54ac16ca2bfe67e
Luc De Meyer <luc.demeyer@noviat.com>
2020-11-27 10:54:29 +0100

    check if fintech lib is installed

--- account_ebics/__init__.py
+++ account_ebics/__init__.py
@@ -1,2 +1,9 @@
-from . import models
-from . import wizards
+import logging
+
+_logger = logging.getLogger(__name__)
+
+try:
+    from . import models
+    from . import wizards
+except Exception:
+    _logger.warning("Import Error, check if fintech lib has been installed")
