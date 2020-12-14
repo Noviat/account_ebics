847eb936e0b65a26f3ffecad5dda382511b04661
Luc De Meyer <luc.demeyer@noviat.com>
2020-12-14 09:32:49 -0800

    [14.0][MIG]account_ebics

---
+++ account_ebics_oe_statement_import/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2020 Noviat.
+# License LGPL-3 or later (http://www.gnu.org/licenses/lpgl).
+
+{
+    'name': 'account_ebics with Odoo Enterprise Bank Statement Import',
+    'summary': "Use Odoo Enterprise Bank Statement Import with account_ebics",
+    'version': '14.0.1.0.0',
+    'author': 'Noviat',
+    'category': 'Hidden',
+    'license': 'LGPL-3',
+    'depends': [
+        'account_ebics_oe',
+        'account_bank_statement_import',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
