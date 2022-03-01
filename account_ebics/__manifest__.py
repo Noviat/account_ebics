fd2e167764febe46bf52d56526615f32569a9ab5
Luc De Meyer <luc.demeyer@noviat.com>
2022-03-01 22:24:12 +0100

    [12.0]fix ebics 2.4 OrderId management

--- account_ebics/__manifest__.py
+++ account_ebics/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright 2009-2020 Noviat.
+# Copyright 2009-2022 Noviat.
 # License LGPL-3 or later (http://www.gnu.org/licenses/lpgl).
 
 {
     'name': 'EBICS banking protocol',
-    'version': '12.0.1.1.0',
+    'version': '12.0.1.1.1',
     'license': 'LGPL-3',
     'author': 'Noviat',
     'category': 'Accounting & Finance',
