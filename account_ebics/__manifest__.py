39d7decb775fa8ad07f3fff6cb7db11ce2a7e23b
Luc De Meyer <luc.demeyer@noviat.com>
2022-03-01 22:24:12 +0100

    [11.0]fix ebics 2.4 OrderId management

--- account_ebics/__manifest__.py
+++ account_ebics/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright 2009-2020 Noviat.
+# Copyright 2009-2022 Noviat.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'EBICS banking protocol',
-    'version': '11.0.1.7.2',
+    'version': '11.0.1.7.3',
     'license': 'AGPL-3',
     'author': 'Noviat',
     'category': 'Accounting & Finance',
--- account_ebics/wizard/ebics_xfer.py
+++ account_ebics/wizard/ebics_xfer.py
@@ -1,4 +1,4 @@
-# Copyright 2009-2019 Noviat.
+# Copyright 2009-2022 Noviat.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 """
@@ -327,8 +327,9 @@ class EbicsXfer(models.TransientModel):
                 tb = ''.join(format_exception(*exc_info()))
                 self.note += '\n%s' % tb
 
-            if self.ebics_config_id.ebics_version == 'H003' and OrderID:
-                self.ebics_config_id._update_order_number(OrderID)
+            if self.ebics_config_id.ebics_version == 'H003':
+                OrderID = self.ebics_config_id._get_order_number()
+                self.ebics_config_id.sudo()._update_order_number(OrderID)
 
         return ebics_file
 
