57adb25c3f7c5a148f9a78b284a0aaa6efed8d5e
Luc De Meyer <luc.demeyer@noviat.com>
2022-05-10 21:40:54 +0200

    [14.0]black, isort, prettier

--- account_ebics/migrations/13.0.1.3/post-migration.py
+++ account_ebics/migrations/13.0.1.3/post-migration.py
@@ -2,16 +2,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 _FILE_FORMATS = [
-    {'xml_id_name': 'ebics_ff_C52',
-     'download_process_method': 'camt.052',
-     },
-    {'xml_id_name': 'ebics_ff_C53',
-     'download_process_method': 'camt.053',
-     },
-    {'xml_id_name': 'ebics_ff_FDL_camt_xxx_cfonb120_stm',
-     'download_process_method': 'cfonb120',
-     },
-
+    {
+        "xml_id_name": "ebics_ff_C52",
+        "download_process_method": "camt.052",
+    },
+    {
+        "xml_id_name": "ebics_ff_C53",
+        "download_process_method": "camt.053",
+    },
+    {
+        "xml_id_name": "ebics_ff_FDL_camt_xxx_cfonb120_stm",
+        "download_process_method": "cfonb120",
+    },
 ]
 
 
@@ -21,21 +23,22 @@ def migrate(cr, version):
 
 
 def _update_file_format(cr, ff):
-    cr.execute(
+    cr.execute(  # pylint: disable=E8103
         """
         SELECT res_id FROM ir_model_data
         WHERE module='account_ebics' AND name='{}'
-        """.format(ff['xml_id_name'])
+        """.format(
+            ff["xml_id_name"]
+        )
     )
     res = cr.fetchone()
     if res:
-        cr.execute(
+        cr.execute(  # pylint: disable=E8103
             """
     UPDATE ebics_file_format
     SET download_process_method='{download_process_method}'
     WHERE id={ff_id};
             """.format(
-                    download_process_method=ff['download_process_method'],
-                    ff_id=res[0]
-                )
+                download_process_method=ff["download_process_method"], ff_id=res[0]
+            )
         )
--- account_ebics/models/account_bank_statement.py
+++ account_ebics/models/account_bank_statement.py
@@ -5,7 +5,6 @@ from odoo import fields, models
 
 
 class AccountBankStatement(models.Model):
-    _inherit = 'account.bank.statement'
+    _inherit = "account.bank.statement"
 
-    ebics_file_id = fields.Many2one(
-        comodel_name='ebics.file', string='EBICS Data File')
+    ebics_file_id = fields.Many2one(comodel_name="ebics.file", string="EBICS Data File")
