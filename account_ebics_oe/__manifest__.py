e921cb5cd5270b081db0d22c812d95d353825cb3
Luc De Meyer <luc.demeyer@noviat.com>
2022-05-10 21:40:54 +0200

    [14.0]black, isort, prettier

--- account_ebics_oe/__manifest__.py
+++ account_ebics_oe/__manifest__.py
@@ -2,19 +2,17 @@
 # License LGPL-3 or later (http://www.gnu.org/licenses/lpgl).
 
 {
-    'name': 'account_ebics on Odoo Enterprise',
-    'summary': "Deploy account_ebics module on Odoo Enterprise",
-    'version': '14.0.1.0.0',
-    'author': 'Noviat',
-    'category': 'Hidden',
-    'license': 'LGPL-3',
-    'depends': [
-        'account_ebics',
-        'account_accountant',
+    "name": "account_ebics on Odoo Enterprise",
+    "summary": "Deploy account_ebics module on Odoo Enterprise",
+    "version": "14.0.1.0.0",
+    "author": "Noviat",
+    "category": "Hidden",
+    "license": "LGPL-3",
+    "depends": [
+        "account_ebics",
+        "account_accountant",
     ],
-    'data': [
-        'views/account_ebics_menu.xml'
-    ],
-    'installable': True,
-    'auto_install': True,
+    "data": ["views/account_ebics_menu.xml"],
+    "installable": True,
+    "auto_install": True,
 }
