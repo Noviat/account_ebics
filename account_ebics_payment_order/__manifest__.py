6e53857434005358308da67c64e3ea63e2ba1b10
Luc De Meyer <luc.demeyer@noviat.com>
2022-05-10 21:40:54 +0200

    [14.0]black, isort, prettier

--- account_ebics_payment_order/__manifest__.py
+++ account_ebics_payment_order/__manifest__.py
@@ -2,16 +2,14 @@
 # License LGPL-3 or later (http://www.gnu.org/licenses/lpgl).
 
 {
-    'name': 'Upload Payment Order via EBICS',
-    'version': '14.0.1.0.0',
-    'license': 'LGPL-3',
-    'author': 'Noviat',
-    'category': 'Accounting & Finance',
-    'depends': [
-        'account_ebics',
-        'account_payment_order'],
-    'data': [
-        'views/account_payment_order.xml',
+    "name": "Upload Payment Order via EBICS",
+    "version": "14.0.1.0.0",
+    "license": "LGPL-3",
+    "author": "Noviat",
+    "category": "Accounting & Finance",
+    "depends": ["account_ebics", "account_payment_order"],
+    "data": [
+        "views/account_payment_order.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
