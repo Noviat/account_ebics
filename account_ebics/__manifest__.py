a713d2e62600e7cd0be95ed7f666525ad4fa77d5
Luc De Meyer <luc.demeyer@noviat.com>
2022-08-05 12:29:47 +0200

    [14.0]account_ebics - fix stack trace when using Odoo OE statement import

--- account_ebics/__manifest__.py
+++ account_ebics/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "EBICS banking protocol",
-    "version": "14.0.1.0.3",
+    "version": "14.0.1.0.4",
     "license": "LGPL-3",
     "author": "Noviat",
     "website": "www.noviat.com",
