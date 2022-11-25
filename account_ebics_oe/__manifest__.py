ae1aeb45eb1fc13a415badd30672b8488c162476
Luc De Meyer <luc.demeyer@noviat.com>
2022-11-25 11:12:38 +0100

    [MIG] account_ebics_oe: Migration to 15.0

--- account_ebics_oe/__manifest__.py
+++ account_ebics_oe/__manifest__.py
@@ -1,11 +1,12 @@
-# Copyright 2020 Noviat.
+# Copyright 2020-2022 Noviat.
 # License LGPL-3 or later (http://www.gnu.org/licenses/lpgl).
 
 {
     "name": "account_ebics on Odoo Enterprise",
     "summary": "Deploy account_ebics module on Odoo Enterprise",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Noviat",
+    "website": "https://www.noviat.com",
     "category": "Hidden",
     "license": "LGPL-3",
     "depends": [
