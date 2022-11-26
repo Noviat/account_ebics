4bae6708e29f67b349518c2e07cdd27c359feea7
Luc De Meyer <luc.demeyer@noviat.com>
2022-11-26 16:42:56 +0100

    [MIG] account_ebics_oca_statement_import: Migration to 15.0

--- account_ebics_oca_statement_import/__manifest__.py
+++ account_ebics_oca_statement_import/__manifest__.py
@@ -1,11 +1,12 @@
-# Copyright 2020 Noviat.
+# Copyright 2020-2022 Noviat.
 # License LGPL-3 or later (http://www.gnu.org/licenses/lpgl).
 
 {
     "name": "account_ebics with OCA Bank Statement Imoort",
     "summary": "Use OCA Bank Statement Import with account_ebics",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "Noviat",
+    "website": "https://www.noviat.com",
     "category": "Hidden",
     "license": "LGPL-3",
     "depends": [
