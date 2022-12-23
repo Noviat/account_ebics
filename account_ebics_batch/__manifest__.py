19cad9d9b3d15c1e9dbe3788c9cce40f418c57d1
Luc De Meyer <luc.demeyer@noviat.com>
2022-12-23 11:46:48 +0100

    [MIG] : Migration to 16.0

--- account_ebics_batch/__manifest__.py
+++ account_ebics_batch/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "EBICS Files batch import",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Noviat",
     "website": "https://www.noviat.com",
