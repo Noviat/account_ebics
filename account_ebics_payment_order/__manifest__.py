04f4f1499870fe9ce5bf4e6286c8b97a7708a587
Jérémy Didderen <jeremy.didderen@noviat.com>
2023-08-13 17:51:31 +0200

    add CI config

--- account_ebics_payment_order/__manifest__.py
+++ account_ebics_payment_order/__manifest__.py
@@ -6,7 +6,7 @@
     "version": "16.0.1.0.0",
     "license": "LGPL-3",
     "author": "Noviat",
-    "website": "https://www.noviat.com",
+    "website": "https://www.noviat.com/",
     "category": "Accounting & Finance",
     "depends": ["account_ebics", "account_payment_order"],
     "data": [
