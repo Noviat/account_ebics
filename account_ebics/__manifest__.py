7da964cd836189fddae455924311cfbf57663b16
Luc De Meyer <luc.demeyer@noviat.com>
2023-09-17 16:14:54 +0200

    [16.0] add support for COMMERZBANK camt files

--- account_ebics/__manifest__.py
+++ account_ebics/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "EBICS banking protocol",
-    "version": "16.0.1.6.0",
+    "version": "16.0.1.6.1",
     "license": "LGPL-3",
     "author": "Noviat",
     "website": "https://www.noviat.com/",
