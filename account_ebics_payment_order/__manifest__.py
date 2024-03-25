ced06fdf9aee42916dc0a79258ad882012323c22
Luc De Meyer <luc.demeyer@noviat.com>
2024-03-25 18:05:57 +0100

    [MIG] account_ebics_payment_order: Migration to 17.0

--- account_ebics_payment_order/__manifest__.py
+++ account_ebics_payment_order/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright 2009-2023 Noviat.
+# Copyright 2009-2024 Noviat.
 # License LGPL-3 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     "name": "Upload Payment Order via EBICS",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "license": "LGPL-3",
     "author": "Noviat",
     "website": "https://www.noviat.com",
