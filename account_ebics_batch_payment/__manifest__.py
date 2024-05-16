ed313c4388d26674effcf6b6f36d32e3681e22d9
Luc De Meyer <luc.demeyer@noviat.com>
2024-05-16 21:45:49 +0200

    [FIX]account_ebics_batch_payment: fix stacktrace in ebics_upload

--- account_ebics_batch_payment/__manifest__.py
+++ account_ebics_batch_payment/__manifest__.py
@@ -1,9 +1,9 @@
-# Copyright 2009-2022 Noviat.
+# Copyright 2009-2024 Noviat.
 # License LGPL-3 or later (http://www.gnu.org/licenses/lpgl).
 
 {
     "name": "Upload Batch Payment via EBICS",
-    "version": "14.0.1.1.0",
+    "version": "14.0.1.1.1",
     "license": "LGPL-3",
     "author": "Noviat",
     "website": "https://www.noviat.com/",
