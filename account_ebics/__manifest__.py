bc2f96f603e0bb07b822c9cf6dc4ddf244c1c6f4
Luc De Meyer <luc.demeyer@noviat.com>
2024-07-15 22:50:51 +0200

    [FIX]account_ebics - fix split cfonb120 files

--- account_ebics/__manifest__.py
+++ account_ebics/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "EBICS banking protocol",
-    "version": "17.0.1.1.0",
+    "version": "17.0.1.1.1",
     "license": "LGPL-3",
     "author": "Noviat",
     "website": "https://www.noviat.com",
