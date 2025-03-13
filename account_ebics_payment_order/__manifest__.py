0cfd7542fde74bed125ba3a640fc8987a705cfbd
Jérémy Didderen <didderen.jeremy@jdi.me>
2025-03-14 00:13:45 +0100

    update ci

--- account_ebics_payment_order/__manifest__.py
+++ account_ebics_payment_order/__manifest__.py
@@ -6,7 +6,7 @@
     "version": "17.0.1.1.0",
     "license": "LGPL-3",
     "author": "Noviat",
-    "website": "https://www.noviat.com",
+    "website": "https://www.noviat.com/",
     "category": "Accounting & Finance",
     "depends": ["account_ebics", "account_payment_order"],
     "data": [
