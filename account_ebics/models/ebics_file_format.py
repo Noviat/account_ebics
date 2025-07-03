2d2f3ae1cbf91607521fd8c3372dc8a84b1757c9
Luc De Meyer <luc.demeyer@noviat.com>
2025-07-03 21:59:33 +0200

    [IMP]account_ebics : improve file format views

--- account_ebics/models/ebics_file_format.py
+++ account_ebics/models/ebics_file_format.py
@@ -1,5 +1,5 @@
 # Copyright 2009-2024 Noviat.
-# License LGPL-3 or later (http://www.gnu.org/licenses/lgpl).
+# License LGPL-3 or later (https://www.gnu.org/licenses/lgpl).
 
 from odoo import api, fields, models
 
