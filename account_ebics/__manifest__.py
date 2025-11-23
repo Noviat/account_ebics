f3151355c3388a6dc0328a85570dd9560ae281d0
Luc De Meyer <luc.demeyer@noviat.com>
2025-11-23 12:40:34 +0100

    [IMP] account_ebics: bump module version

--- account_ebics/__manifest__.py
+++ account_ebics/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "EBICS banking protocol",
-    "version": "18.0.1.1.0",
+    "version": "18.0.1.1.1",
     "license": "LGPL-3",
     "author": "Noviat",
     "website": "https://www.noviat.com/",
