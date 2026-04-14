3d66ac3bd658e5bf67865c8c2ae47137586edc6d
Luc De Meyer <luc.demeyer@noviat.com>
2026-04-14 15:52:51 +0200

    [IMP] batch payment: hide ebics_upload button on non-ebics journals

--- account_ebics_batch_payment/models/account_batch_payment.py
+++ account_ebics_batch_payment/models/account_batch_payment.py
@@ -1,34 +1,37 @@
-# Copyright 2009-2024 Noviat.
+# Copyright 2020 Noviat.
 # License LGPL-3 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import models
+from odoo import fields, models
 from odoo.exceptions import UserError
 
 
 class AccountBatchPayment(models.Model):
     _inherit = "account.batch.payment"
 
+    hide_ebics_upload = fields.Boolean(compute="_compute_hide_ebics_upload")
+
+    def _compute_hide_ebics_upload(self):
+        for rec in self:
+            rec.hide_ebics_upload = (
+                not rec.journal_id.ebics_config_id
+                or not rec.file_generation_enabled
+                or rec.state != "sent"
+            )
+
     def ebics_upload(self):
         self.ensure_one()
         ctx = self.env.context.copy()
 
         origin = self.env._("Batch Payment") + ": " + self.name
-        ebics_config = self.env["ebics.config"].search(
-            [
-                ("journal_ids", "=", self.journal_id.id),
-                ("state", "=", "confirm"),
-            ]
-        )
-        if not ebics_config:
+        if not self.journal_id.ebics_config_id:
             raise UserError(
                 self.env._(
                     "No active EBICS configuration available for the selected bank."
                 )
             )
-        if len(ebics_config) == 1:
-            ctx["default_ebics_config_id"] = ebics_config.id
         ctx.update(
             {
+                "default_ebics_config_id": self.journal_id.ebics_config_id.id,
                 "default_upload_data": self.export_file,
                 "default_upload_fname": self.export_filename,
                 "origin": origin,
