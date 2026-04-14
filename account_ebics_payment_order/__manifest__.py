6cd9edba405f085546b66e4dbc19458deaa982e4
Luc De Meyer <luc.demeyer@noviat.com>
2026-04-14 21:57:00 +0200

    [IMP] account_ebics, account_ebics_batch_payment, account_ebics_payment_order: bump module versions

--- account_ebics_payment_order/__manifest__.py
+++ account_ebics_payment_order/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Upload Payment Order via EBICS",
-    "version": "18.0.1.1.0",
+    "version": "18.0.1.2.0",
     "license": "LGPL-3",
     "author": "Noviat",
     "website": "https://www.noviat.com/",
