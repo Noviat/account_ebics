6e7359bbde187e0e268b381500ab095a7d7e9efe
Luc De Meyer <luc.demeyer@noviat.com>
2026-08-04 17:20:10 +0200

    [IMP] account_ebics_batch_payment: pre-fill EBICS file format from payment method

--- account_ebics_batch_payment/__manifest__.py
+++ account_ebics_batch_payment/__manifest__.py
@@ -3,13 +3,16 @@
 
 {
     "name": "Upload Batch Payment via EBICS",
-    "version": "18.0.1.1.0",
+    "version": "18.0.1.2.0",
     "license": "LGPL-3",
     "author": "Noviat",
     "website": "https://www.noviat.com/",
     "category": "Accounting & Finance",
     "depends": ["account_ebics", "account_batch_payment"],
-    "data": ["views/account_batch_payment_views.xml"],
+    "data": [
+        "views/account_journal_views.xml",
+        "views/account_batch_payment_views.xml",
+    ],
     "installable": True,
     "images": ["static/description/cover.png"],
 }
